fe7d0190bc18e09f6d7dd50e636bf9c6e15b4a31
Samuel Cornet <samuel@noukod.com>
2018-09-21 13:15:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 def prime?(num)
   if num > 1
-    range = (2...num -1).to_a
+    range = (2...num).to_a
     range.none? do |divisor|
      num % divisor == 0
   end
-else
-
-  false
-end
+  else
+    false
+  end
 end
